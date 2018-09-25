2e415eb654aef9b4b6ca1a757ac2dd33f0bdbab0
lindawwww <s1230221@gmail.com>
2018-09-25 14:48:50 +0900

    Fix: replace  .innerHTML  into .textContent

--- docs/assets/js/printOrderCard.js
+++ docs/assets/js/printOrderCard.js
@@ -2,19 +2,19 @@
   var storage = sessionStorage;
 
   console.log(storage.getItem("orderName"));
-  document.getElementById("order_name").innerHTML = storage.getItem("orderName");
+  document.getElementById("order_name").textContent = storage.getItem("orderName");
   console.log(storage.getItem("orderRepresentative"));
-  document.getElementById("order_representative").innerHTML = storage.getItem("orderRepresentative");
+  document.getElementById("order_representative").textContent = storage.getItem("orderRepresentative");
   console.log(storage.getItem("orderCustomerRepresentative"));
-  document.getElementById("order_customer_representative").innerHTML = storage.getItem("orderCustomerRepresentative");
+  document.getElementById("order_customer_representative").textContent = storage.getItem("orderCustomerRepresentative");
   console.log(storage.getItem("orderDescription"));
-  document.getElementById("order_description").innerHTML = storage.getItem("orderDescription");
+  document.getElementById("order_description").textContent = storage.getItem("orderDescription");
   console.log(storage.getItem("orderTotalCost"));
-  document.getElementById("order_cost").innerHTML = storage.getItem("orderTotalCost");
+  document.getElementById("order_cost").textContent = storage.getItem("orderTotalCost");
   console.log(storage.getItem("orderTotalUnitPrice"));
-  document.getElementById("order_total_unit_price").innerHTML = storage.getItem("orderTotalUnitPrice");
+  document.getElementById("order_total_unit_price").textContent = storage.getItem("orderTotalUnitPrice");
   console.log(storage.getItem("dateReceivedOrder"));
-  document.getElementById("date_received_order").innerHTML = storage.getItem("dateReceivedOrder");
+  document.getElementById("date_received_order").textContent = storage.getItem("dateReceivedOrder");
 
 
   // var numberOfProductAttachments = storage.getItem("numberOfProductAttachments");
