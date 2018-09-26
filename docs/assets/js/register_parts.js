891b704523ee340f78aa0ae710e1f851e031e779
lindawwww <s1230221@gmail.com>
2018-09-26 13:56:32 +0900

    change the time to display for notice it's okay

--- docs/assets/js/register_parts.js
+++ docs/assets/js/register_parts.js
@@ -29,7 +29,7 @@ const partsBoard = TrelloBoards.Parts;
 
 const informAdd = function(){
 		$(".message:not(:animated)").fadeIn("slow",function(){
-			$(this).delay(1000).fadeOut("slow");
+			$(this).delay(1300).fadeOut("slow");
 		});
 }
 
